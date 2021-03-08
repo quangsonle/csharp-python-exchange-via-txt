8e567f21117d8213539cadb88fd992f86c8ba416
Quang Sơn Le <48610518+quangsonle@users.noreply.github.com>
2021-03-08 16:02:54 +0200

    Update Form1.cs

--- WindowsFormsApplication1/Form1.cs
+++ WindowsFormsApplication1/Form1.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Windows.Forms;
 using IronPython.Hosting;
 using System.IO;
@@ -20,38 +20,38 @@ namespace WindowsFormsApplication1
 
         private void button1_Click(object sender, EventArgs e)
         {
-            
+
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
 
 
-           
+
 
         }
 
         private void textBox1_TextChanged(object sender, EventArgs e)
         {
-            using (StreamWriter writer = new StreamWriter(@"C:\Users\user\Desktop\rl_human\test.txt"))
+            using (StreamWriter writer = new StreamWriter(@"C:\Users\admin\Desktop\project\github_working_place\csharp-python-exchange-via-txt-master\test.txt"))
             {
                 writer.Write(textBox1.Text);
 
             }
         }
 
-       
+
         private void Form1_KeyPress(object sender, KeyPressEventArgs e)
         {
-            Console.WriteLine("co phim dc nhan");
-            Console.WriteLine(e.KeyChar);
-            
-            using (StreamWriter writer = new StreamWriter(@"C:\Users\user\Desktop\rl_human\test.txt"))
+            //Console.WriteLine("co phim dc nhan");
+          //  Console.WriteLine(e.KeyChar);
+
+            using (StreamWriter writer = new StreamWriter(@"C:\Users\admin\Desktop\project\github_working_place\csharp-python-exchange-via-txt-master\test.txt"))
             {
 
-                   
-                        writer.Write(e.KeyChar);
-                    
+
+               writer.Write(e.KeyChar);
+
 
             }
 
@@ -70,13 +70,25 @@ namespace WindowsFormsApplication1
 
         private void timer1_Tick(object sender, EventArgs e)
         {
-            using (var fs = new FileStream(@"C:\Users\user\Desktop\rl_human\test2.txt", FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+
+            using (var fs = new FileStream(@"C: \Users\admin\Desktop\project\github_working_place\csharp-python-exchange-via-txt-master\test2.txt", FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
             using (var reader = new StreamReader(fs, System.Text.Encoding.Default))
             {
+                int feedback = 0;
+                string readered = reader.ReadToEnd();
+                if (!String.IsNullOrEmpty(readered))
+                {
 
-                Console.WriteLine(reader.ReadToEnd());
-                //  label1.Text = text;
+
+
+
+
+                    feedback = Int32.Parse(readered);
+                    // Console.WriteLine("afdsfd is: " + feedback.ToString());
+                    label1.Text = feedback.ToString();
+                }
             }
         }
     }
 }
+
